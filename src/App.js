6a2fc85a8a8259593ab2426af08679007d7cea97
Akash Sahu <akash.sahu@nordcloud.com>
2022-04-21 07:59:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,142 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const url = "http://localhost:3005/";
+  const [newItem, setnewItem] = useState("");
+  const [list, setList] = useState([]);
+
+  function updateInput(event) {
+    event.preventDefault();
+    setnewItem(event.target.value);
+  }
+
+  const updateStatus = (e, id, itemStatus) => {
+    fetch(`${url}modify/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        status: itemStatus === "Pending" ? "Done" : "Pending",
+      }),
+    })
+      .then(() => getItems())
+      .catch((err) => console.log(err));
+  };
+
+  const deleteItem = (id) => {
+    fetch(`${url}delete`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: id }),
+    })
+      .then(() => getItems())
+      .catch((err) => console.log(err));
+  };
+
+  const addItem = (data) => {
+    fetch(`${url}store-data`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ item: data }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then((res) => {
+        if (res.success === true) setnewItem("");
+        getItems();
+      })
+      .catch((err) => console.log(err));
+  };
+  function getItems() {
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then((myJson) => setList(myJson))
+      .catch((err) => console.log(err));
+  }
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div data-testid="todoapp">
+      <h1 className="app-title">MY TODO LIST</h1>
+
+      <div className="container">
+        <div className="wrapper">
+          <h2>Add an Item...</h2>
+          <br />
+          <input
+            data-testid="input"
+            className="InputBox"
+            type="text"
+            placeholder="Type item here"
+            value={newItem}
+            onChange={(e) => updateInput(e)}
+          />
+          <button
+            className="btn-add"
+            onClick={(e) => addItem(newItem)}
+            disabled={!newItem.length}
+            data-testid="addButton"
+          >
+            ADD
+          </button>
+        </div>
+
+        <br />
+
+        <ul data-testid="TodoList">
+          
+            {list.map((item) => (
+              <li key={item.id} className="Partcontainer" data-testid="todo">
+                <div >
+                <h3
+                  className="List-Item"
+                  style={{
+                    textDecoration:
+                      item.status === "Done" ? "line-through" : "none",
+                  }}
+                  onClick={(e) => updateStatus(e, item.id, item.status)}
+                >
+                  {item.item}
+                </h3>
+                <button
+                  data-testid="deleteButton"
+                  className="btn"
+                  onClick={() => deleteItem(item.id)}
+                >
+                  <i>x</i>
+                </button></div>
+              </li>
+            ))}
+          
+        </ul>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,63 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent, cleanup, screen } from "@testing-library/react";
+import App from "./App";
+import * as React from "react";
 
-test('renders learn react link', () => {
+// HACK TO MAKE CODESANDBOX WORK
+import expect from "expect";
+global.expect = expect;
+// require('jest-dom/extend-expect');
+
+test("TODO application is rendered", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/MY TODO LIST/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("renders the correct initial DOM", () => {
+  const doc = render(<App />);
+  const inputElement = doc.getByTestId("input");
+  // The input should be blank.
+  expect(inputElement.getAttribute("value")).toBe("");
+});
+
+test("creates a new todo", async () => {
+  const doc = render(<App />);
+
+  const inputElement = doc.getByTestId("input");
+  const createButtonElement = doc.getByTestId("addButton");
+
+  // Create the todo.
+  fireEvent.change(inputElement, { target: { value: "Feed my dog." } });
+  fireEvent.click(createButtonElement);
+
+  const todos = doc.getAllByTestId('TodoList');
+  const todo = doc.getByTestId('todo');
+  const todoNameElement = todo.lastChild;
+ 
+ console.log('everfirst chileything',todoNameElement);
+  // The name should be in the document as "Feed my dog."
+  expect(todoNameElement).toBeInTheDocument();
+  expect(todoNameElement).toHaveTextContent("Feed my dog.");
+
+});
+
+// // test2: Make sure that after creating a todo, if the
+// // user clicks the delete button, a todo goes away.
+// test("it deletes a todo", () => {
+//   const doc = render(<App />);
+
+//   const inputElement = doc.getByTestId("input");
+//   const createButtonElement = doc.getByTestId("addButton");
+
+//   // Create the todo.
+//   fireEvent.change(inputElement, { target: { value: "Feed my cat." } });
+//   fireEvent.click(createButtonElement);
+
+//   // Get the newly created todo.
+//   const todo = doc.queryByTestId("todo");
+
+//   // Click the delete button on the todo.
+//   const todoDeleteButton = doc.getByTestId("deleteButton");
+//   fireEvent.click(todoDeleteButton);
+
+// });
